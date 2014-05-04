f5a98493ee7d64d7789efa62df2a18b14c234c7b
Iain Emsley <iain_emsley@austgate.co.uk>
2014-05-04 13:39:15 +0100

    Updated notes URL for Wordpress API

--- textus.php
+++ textus.php
@@ -82,6 +82,7 @@ function textus_shortcode( $atts ) {
     );
     $rawtext = "/wordpress/wp-content/uploads/".$atts['id']."-text.txt";   
     $rawjson = "/wordpress/wp-content/uploads/".$atts['id']."-typography.json";
+    $notes_id = split('/',$atts['id']);
     // return the text with the call the the Javascript location
     return '<div id=\'textViewDiv\'></div>
 <!-- Textus JS dependencies -->
@@ -107,7 +108,7 @@ jQuery(document).ready(function() {
     id: \'text-1\',
     textUrl: "'.$rawtext.'",
     typographyUrl: "'.$rawjson.'",
-    annotationsUrl: "http://localhost/wordpress/?text='.$atts['id'].'&type=annotation"
+    annotationsUrl: "http://localhost/wordpress/?text='.$notes_id[1].'&type=annotation"
   });
   // Load the text
   text.fetch(function(err) {
